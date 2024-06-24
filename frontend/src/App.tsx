3992c6e01d783a1a0eb0a9880f70386398613f2a
Nadav Somekh <nadavys@gmail.com>
2024-06-24 09:39:54 -0700

    footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@
 import './App.css'
 import SongList from './components/SongList'
 import retroBg from './assets/retro-bg.jpg';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <SongList />
         </div>
       </div>
+      <Footer/>
     </>
   )
 }
