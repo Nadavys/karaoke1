61e2a2d4526e35e09e304b9b4920016515867475
Nadav Somekh <nadavys@gmail.com>
2024-06-24 01:16:05 -0700

    gh actions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import SongList from './components/SongList'
--- frontend/src/components/SongList.tsx
+++ frontend/src/components/SongList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Video } from "../SongContext";
 
 import { useSongList } from '../SongContext';
