b494988dac01fcbcc40794e01565e128df623cba
Nadav Somekh <nadavys@gmail.com>
2024-06-24 10:05:16 -0700

    footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Footer = () => {
+const Footer: React.FC = () => {
 const currentYear = new Date().getFullYear(); 
   return (
     <footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
