18a701d18994b9bbb1eacbc696efeb669831dd52
Nadav Somekh <nadavys@gmail.com>
2024-06-24 12:28:08 -0700

    sign

--- frontend/src/components/SongList.tsx
+++ frontend/src/components/SongList.tsx
@@ -41,7 +41,7 @@ function SongList() {
   return (
 
     <div>
-      <h1 className='text-white text-center text-xl pt-2'>Karaoke Party</h1>
+      <h1 className='text-white text-center text-5xl pt-2 text-pink-200' style={{ fontFamily: 'Cursive', textShadow: '0 0 20px #FFF, 2px 2px 4px rgba(0, 0, 0, 1)' }} >Karaoke Party</h1>
 
       <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
         <div className="md:flex">
