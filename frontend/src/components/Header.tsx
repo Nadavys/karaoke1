99f63d7919290b7c1bda7f5d39082a2df954c094
Nadav Somekh <nadavys@gmail.com>
2024-06-26 00:05:11 -0700

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC<HeaderProps> = ({ searchQuery, setSearchQuery, foundVideo
     };
 
     return (
-        <header className="sticky top-0 shadow-md bg-slate-800 bg-opacity-50 backdrop-blur-lg py-2">
+        <header className="sticky top-0 shadow-md _bg-slate-800 bg-opacity-50 backdrop-blur-lg backdrop-saturate-50 py-2">
             <div className="flex container mx-auto">
                 {/* <h1 className='text-white text-center text-5xl pt-2 text-pink-200' style={{ fontFamily: 'Cursive', textShadow: '0 0 20px #FFF, 2px 2px 4px rgba(0, 0, 0, 1)' }} >Karaoke Party</h1> */}
                 <img src={logoImg} alt="logo" className="px-1 max-h-[75px]" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} />
