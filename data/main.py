1e1a512f66bab510433880f24e1537d5355aa5d5
Nadav Somekh <nadavys@gmail.com>
2024-07-19 21:45:15 -0700

    new songs

--- data/main.py
+++ data/main.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 from googleapiclient.discovery import build
 import json
 from youtube import youtube_search, get_video
-from db import insert_data, db_close, make_json_file
-
+from db import insert_data, db_close, make_json_file, conn
+from psycopg2 import errors
+# import psycopg2.errors.UniqueViolation
 def main():
     load_dotenv()
     
@@ -15,7 +16,7 @@ def main():
     # db.create_table()
     
 
-    make_json_file()
+    # make_json_file()
     collect()   
         
     db_close()
@@ -23,13 +24,26 @@ def main():
     
 def collect():
     while True:
-        video_id = input("Enter a video ID (q to quit): ")  
+        video_id = input("Enter a video ID (q to quit, m to make data file): ")  
             
         if video_id.lower() == 'q':
             print("Exiting program...")
             break
+        
+        if video_id.lower() == 'm':
+            make_json_file()   
+            print("New Data file created. Exiting program.")
+            break
+        
         data = get_video(video_id)
-        insert_data(data['id'], data['title'], data['description'], data['thumbnail'], data['channelTitle'], data['publishTime'])
+        try:
+            insert_data(data['id'], data['title'], data['description'], data['thumbnail'], data['channelTitle'], data['publishTime'])
+        except errors.UniqueViolation as e:
+            # end last transaction from the data insert
+            conn.rollback()
+            print(f"*******************************************")
+            print(f"** This video is already in the database. {data['title']}**")
+            print(f"*******************************************")
 
     
 if __name__ == "__main__":
