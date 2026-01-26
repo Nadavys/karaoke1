1c178a60b6d4950a4e1ef92b10dc944ab608a665
Nadav Somekh <nadavys@gmail.com>
2026-01-26 11:28:58 -0800

    add tracking for page views with ReactGA in Layout component

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -4,9 +4,17 @@ import './App.css'
 import retroBg from './assets/retro-bg.jpg';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 function Layout({children}: { children: ReactNode }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
+
   return (
     <>
       <div className="min-h-screen w-full pb-4" style={{ backgroundImage: `url(${retroBg})`, backgroundRepeat: 'repeat-x', backgroundSize: 'contains', backgroundColor: '#5E165F', backgroundPosition: "top" }}>
