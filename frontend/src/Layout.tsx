b3046e043829fbf3ea3c4b29ebe52f8dadd4ff7a
nadav <nadavys@gmail.com>
2026-01-26 12:10:36 -0800

    remove Footer component from Layout and adjust structure

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,7 +3,6 @@
 import './App.css'
 import retroBg from './assets/retro-bg.jpg';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import { ReactNode, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
@@ -16,13 +15,10 @@ function Layout({children}: { children: ReactNode }) {
   }, [location]);
 
   return (
-    <>
-      <div className="min-h-screen w-full pb-4" style={{ backgroundImage: `url(${retroBg})`, backgroundRepeat: 'repeat-x', backgroundSize: 'contains', backgroundColor: '#5E165F', backgroundPosition: "top" }}>
-        <Header/>
-        {children}
-      </div>
-      <Footer />
-    </>
+    <div className="min-h-screen w-full pb-4" style={{ backgroundImage: `url(${retroBg})`, backgroundRepeat: 'repeat-x', backgroundSize: 'contains', backgroundColor: '#5E165F', backgroundPosition: "top", backgroundAttachment: 'fixed' }}>
+      <Header/>
+      {children}
+    </div>
   )
 }
 
